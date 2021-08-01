aeb0bdcefa87670ae07df476db41fbb1f0cdf96b
Manveen80 <manveen77@gmail.com>
2021-08-01 01:37:48 -0700

    Initial commit

---
+++ new.js
@@ -0,0 +1,11 @@
+var x= window.confirm("Do you want to see the puppy?");
+if (x= true) {
+    console.log("you pressed ok");
+    document.write('<img src ="images/pet.jpg ">');
+}
+else
+{
+    console.log("you pressed cancel");
+    document.write('<img src = "images/sad_face.jpg" >');
+
+}
---
+++ new2.js
@@ -0,0 +1,5 @@
+function fn1() {
+    var str = document.getElementById("text1").value;
+    alert("value inside the text box is:" +str);
+
+}
